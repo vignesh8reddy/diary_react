d02947a94399b4fdc148fb892af0563e63e638b4
Vignesh Reddy Ireddy <144072079+vignesh8reddy@users.noreply.github.com>
2024-12-11 21:55:45 -0600

    Commit by Vignesh

--- react-05DataBindingOneWay/src/databinding1.js
+++
@@ -1,8 +0,0 @@
-var categories = ["All", "Electronics", "Footwear"];
-function bodyload() {
-    for(var item of categories) {
-        var li = document.createElement("li");
-        li.innerHTML = item;
-        document.querySelector("ol").appendChild(li);
-    }
-}
